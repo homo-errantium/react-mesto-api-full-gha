2cb5ad827130da3c71b9684545b074a077ff64dc
homo-errantium <homo.errantium@gmail.com>
2023-08-08 15:46:39 +0300

    install VM

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
+// eslint-disable-next-line import/no-extraneous-dependencies
 const cors = require('cors');
 
-const { PORT = 4000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 4000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } =
+  process.env;
 
 const options = {
   origin: ['http://localhost:3000'],
