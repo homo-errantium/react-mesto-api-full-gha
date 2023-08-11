b4d63a7d503fdb8d93fc4da0a7c5c4430d3f39ed
homo-errantium <homo.errantium@gmail.com>
2023-08-11 20:20:45 +0300

    saved last

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,7 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.en
 const options = {
   origin: [
     'http://localhost:3000',
+    'http://localhost:3001',
     'http://my.place.nomoreparties.co',
     'https://my.place.nomoreparties.co',
     'http://api.my.place.nomoreparties.co',
